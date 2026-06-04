9ec1d3ada7dfee3e194dfebf514707727ba7da9f
Leonardo Victor <leonardovictormarques@gmail.com>
2026-06-04 14:42:15 -0300

    react router initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from 'react-router';
+import { RouterProvider } from 'react-router/dom';
 import './index.css';
 import App from './App.tsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    children: [
+      { index: true, Component: App },
+      { path: 'auth', element: <div>Auth Page goes here!</div> },
+      { path: 'artist/:artistId', element: <div>Artist Page goes here!</div> },
+    ],
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
